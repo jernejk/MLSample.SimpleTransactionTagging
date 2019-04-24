37b680d7578b8de7e0d75d04b275268947b8b6e1
Jernej Kavka (JK) <jernejkavka@ssw.com.au>
2019-04-24 17:23:15 +1000

    Upgraded from 0.11 to ML.NET 1.0 preview.

--- MLSample.TransactionTagging/BankTransactionTrainingService.cs
+++ MLSample.TransactionTagging/BankTransactionTrainingService.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.ML;
-using Microsoft.ML.Data;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 namespace MLSample.TransactionTagging
@@ -11,19 +9,24 @@ namespace MLSample.TransactionTagging
         public void Train(IEnumerable<TransactionData> trainingData, string modelSavePath)
         {
             var mlContext = new MLContext(seed: 0);
-            var pipeline = LoadDataPipeline(mlContext);
-            var trainingPipeline = GetTrainingPipeline(mlContext, pipeline);
-            var trainingModel = BuildAndTrainModel(mlContext, trainingPipeline, trainingData);
 
-            using (var fs = new FileStream(modelSavePath, FileMode.Create, FileAccess.Write, FileShare.Write))
-                mlContext.Model.Save(trainingModel, fs);
+            // Configure ML pipeline
+            var pipeline = LoadDataProcessPipeline(mlContext);
+            var trainingPipeline = GetTrainingPipeline(mlContext, pipeline);
+            var trainingDataView = mlContext.Data.LoadFromEnumerable(trainingData);
+
+            // Generate training model.
+            var trainingModel = trainingPipeline.Fit(trainingDataView);
+
+            // Save training model to disk.
+            mlContext.Model.Save(trainingModel, trainingDataView.Schema, modelSavePath);
         }
 
-        private IEstimator<ITransformer> LoadDataPipeline(MLContext mlContext)
+        private IEstimator<ITransformer> LoadDataProcessPipeline(MLContext mlContext)
         {
             // Configure data pipeline based on the features in TransactionData.
             // Description and TransactionType are the inputs and Category is the expected result.
-            var pipeline = mlContext
+            var dataProcessPipeline = mlContext
                 .Transforms.Conversion.MapValueToKey(inputColumnName: nameof(TransactionData.Category), outputColumnName: "Label")
                 .Append(mlContext.Transforms.Text.FeaturizeText(inputColumnName: nameof(TransactionData.Description), outputColumnName: "TitleFeaturized"))
                 .Append(mlContext.Transforms.Text.FeaturizeText(inputColumnName: nameof(TransactionData.TransactionType), outputColumnName: "DescriptionFeaturized"))
@@ -31,7 +34,7 @@ namespace MLSample.TransactionTagging
                 .Append(mlContext.Transforms.Concatenate("Features", "TitleFeaturized", "DescriptionFeaturized"))
                 .AppendCacheCheckpoint(mlContext);
 
-            return pipeline;
+            return dataProcessPipeline;
         }
 
         private IEstimator<ITransformer> GetTrainingPipeline(MLContext mlContext, IEstimator<ITransformer> pipeline)
@@ -39,15 +42,26 @@ namespace MLSample.TransactionTagging
             // Use the multi-class SDCA algorithm to predict the label using features.
             // For StochasticDualCoordinateAscent the KeyToValue needs to be PredictedLabel.
             return pipeline
-                .Append(mlContext.MulticlassClassification.Trainers.StochasticDualCoordinateAscent(DefaultColumnNames.Label, DefaultColumnNames.Features))
+                .Append(GetScadaTrainer(mlContext))
                 .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));
         }
 
-        private ITransformer BuildAndTrainModel(MLContext mlContext, IEstimator<ITransformer> trainingPipeline, IEnumerable<TransactionData> trainingData)
+        private IEstimator<ITransformer> GetScadaTrainer(MLContext mlContext)
         {
-            // Load training data and train the model based on training pipeline.
-            var trainingDataView = mlContext.Data.LoadFromEnumerable(trainingData);
-            return trainingPipeline.Fit(trainingDataView);
+            return mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features");
+        }
+
+        // Alternative trainer
+        private IEstimator<ITransformer> GetAveragedPerceptronTrainer(MLContext mlContext)
+        {
+            // Create a binary classification trainer.
+            var averagedPerceptronBinaryTrainer = mlContext.BinaryClassification.Trainers.AveragedPerceptron("Label", "Features", numberOfIterations: 10);
+
+            // Compose an OVA (One-Versus-All) trainer with the BinaryTrainer.
+            // In this strategy, a binary classification algorithm is used to train one classifier for each class, "
+            // which distinguishes that class from all other classes. Prediction is then performed by running these binary classifiers, "
+            // and choosing the prediction with the highest confidence score.
+            return mlContext.MulticlassClassification.Trainers.OneVersusAll(averagedPerceptronBinaryTrainer);
         }
     }
 }
