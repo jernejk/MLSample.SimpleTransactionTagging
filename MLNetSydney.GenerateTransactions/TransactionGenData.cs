d690bffa104d7db07c82c1cd08247a2f60568fb1
Jernej Kavka (JK) <jk@ssw.com.au>
2021-11-11 21:37:45 +1000

    ✨ Added auto generated financial data.

---
+++ MLNetSydney.GenerateTransactions/TransactionGenData.cs
@@ -0,0 +1,32 @@
+﻿namespace MLNetSydney.GenerateTransactions
+{
+    internal class TransactionGenData
+    {
+        public TransactionGenData() { }
+        public TransactionGenData(string companyName, string category)
+        {
+            CompanyName = companyName;
+            Category = category;
+        }
+
+        public string CompanyName { get; set; }
+        public string Category { get; set; }
+        public TransactionFormatType TransactionFormatType { get; set; }
+
+        public override string ToString()
+        {
+            return $"{CompanyName} - {Category} - {TransactionFormatType}";
+        }
+    }
+
+    public enum TransactionFormatType
+    {
+        CompanyName,
+        CompanyNameWithTransferNumber,
+        CardTypeCompanyName,
+        CardTypeCompanyNameWithTransferNumber,
+        PayPal,
+        Payment,
+        PosAuthorization
+    }
+}
